27ba0c23ee42996168bab066074af069108e1e20
mblackbeak <bethmralston@gmail>
2024-07-14 17:07:49 -0700

    added basic file for creating a new set

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
         <div class="container"><a class="navbar-brand" href="#page-top">Ralston</a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-align-justify"></i></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="#about">Create</a></li>
+                    <li class="nav-item"><a class="nav-link" href="create_set.php">Create</a></li>
                     <li class="nav-item"><a class="nav-link" href="sets.php">FLashCards</a></li>
                     <li class="nav-item"><a class="nav-link" href="#portfolio">QUIzes</a></li>
                     <li class="nav-item"><a class="nav-link" href="index.php">Contact</a></li>
